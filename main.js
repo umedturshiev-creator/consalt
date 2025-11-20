dd87b70439a286890ea9f898fb3f134c176bbfab
umedturshiev-creator <umedturshiev@gmail.com>
2025-11-20 09:35:29 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-document.getElementById('myForm').addEventListener('submit', async function(e) {
-    e.preventDefault();
 
-    const formData = new FormData(this);
-    const obj = Object.fromEntries(formData.entries());
+function sendData() {
+  const inn = document.getElementById("inn").value;
+  const fio = document.getElementById("fio").value;
+  const phone = document.getElementById("phone").value;
+  const income = document.getElementById("income").value;
+  const employees = document.getElementById("employees").value;
+  const month = document.getElementById("month").value;
 
-    let response = await fetch('https://script.google.com/macros/s/AKfycbzKMbvmwKkZeQWjfEShXohJHCwFkzcbvUEQv6k2ViGlQrGDXuhsRXn1ZBtmKY9EDpzB/exec', {
-        method: 'POST',
-        mode: 'no-cors',
-        body: JSON.stringify(obj)
-    });
+  fetch("https://script.google.com/macros/s/AKfycbzKMbvmwKkZeQWjfEShXohJHCwFkzcbvUEQv6k2ViGlQrGDXuhsRXn1ZBtmKY9EDpzB/exec", {
+    method: "POST",
+    mode: "no-cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ inn, fio, phone, income, employees, month })
+  });
 
-    document.getElementById('result').innerText = "Данные отправлены!";
-});
+  document.getElementById("inn").value = "";
+  document.getElementById("fio").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("income").value = "";
+  document.getElementById("employees").value = "";
+  document.getElementById("month").value = "";
+
+  const msg = document.getElementById("successMessage");
+  msg.classList.add("show");
+
+  setTimeout(() => {
+    msg.classList.remove("show");
+  }, 3000);
+}
