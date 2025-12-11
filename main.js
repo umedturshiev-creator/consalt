1c512c4862acd4a5ecbd5065e76ccd6f39eb83f6
umedturshiev-creator <umedturshiev@gmail.com>
2025-12-11 15:57:00 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // ====== URL твоего Apps Script WebApp ======
-const API_URL = "https://script.google.com/macros/s/AKfycbw57_6uKs2-I6rh1S4a560b9YS3P4ufULgx4Mcu8USIPguNxCk6nVUALwWkIdhr5JMWgw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbw57_6uKs2-l6rh1S4a56Ob9YS3P4ufULgKEajqFMR4XNKUdmzlCrDqohgwF5rtyGL/exec";
 
 
 // ====== ЭЛЕМЕНТЫ ФОРМЫ ======
