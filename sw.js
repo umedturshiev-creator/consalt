308630b3ad23351d53f09baf9f414434c445a4d9
umedturshiev-creator <umedturshiev@gmail.com>
2026-02-19 00:52:01 +0500

    Modify service worker cache configuration
    Updated cache name and added new icons to cache.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ua-consulting-v1";
+const CACHE_NAME = "ua-v1";
 
 const urlsToCache = [
   "/",
@@ -6,27 +6,14 @@ const urlsToCache = [
   "/style.css",
   "/main.js",
   "/logo.png",
-  "/manifest.json"
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
   );
 });
 
