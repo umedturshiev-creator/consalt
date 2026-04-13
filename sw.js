36bcf7fd116ffc186f97e72163ebbff7748f8f9f
umedturshiev-creator <umedturshiev@gmail.com>
2026-04-13 08:20:54 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "uza-app-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(["./", "./index.html"]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
